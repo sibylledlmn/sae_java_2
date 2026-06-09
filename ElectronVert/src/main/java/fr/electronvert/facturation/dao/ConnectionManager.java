6f3dad0c1dc90a44d9a4f7ffdf68e76646c09d3a
Sibylle <dillmann.sibylle@gmail.com>
2026-06-09 22:44:23 +0200

    add ConnectionManager with properties file configuration

---
+++ ElectronVert/src/main/java/fr/electronvert/facturation/dao/ConnectionManager.java
@@ -0,0 +1,29 @@
+package fr.electronvert.facturation.dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class ConnectionManager {
+
+    private ConnectionManager() {}
+
+    public static Connection getConnection() throws SQLException {
+        Properties prop = new Properties();
+        try {
+            prop.load(ConnectionManager.class.getClassLoader().getResourceAsStream("db.properties"));
+            String url = prop.getProperty("db.url");
+            String user = prop.getProperty("db.user");
+            String password = prop.getProperty("db.password");
+            return DriverManager.getConnection(url, user, password);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Impossible de charger db.properties", e );
+        }
+    }
+
+
+}
