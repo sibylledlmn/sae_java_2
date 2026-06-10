af2f8b6ce03a5ad919f98a812da63b291a9f997e
Sibylle <dillmann.sibylle@gmail.com>
2026-06-10 19:29:08 +0200

    feat(dao): add ReleveDAO interface and ReleveDAOJdbc implementation

---
+++ ElectronVert/src/main/java/fr/electronvert/facturation/dao/ReleveDAO.java
@@ -0,0 +1,14 @@
+package fr.electronvert.facturation.dao;
+
+import fr.electronvert.facturation.model.releve.Releve;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ReleveDAO {
+
+    int save(Releve releve, int contratId) throws SQLException;
+    List<Releve> findByContractId(int contractId) throws SQLException;
+    Releve findDernerByContractId(int contractId) throws SQLException;
+
+}
---
+++ ElectronVert/src/main/java/fr/electronvert/facturation/dao/impl/ReleveDAOJdbc.java
@@ -0,0 +1,84 @@
+package fr.electronvert.facturation.dao.impl;
+
+import fr.electronvert.facturation.dao.ConnectionManager;
+import fr.electronvert.facturation.dao.ReleveDAO;
+import fr.electronvert.facturation.model.releve.Releve;
+import fr.electronvert.facturation.model.releve.TypeConso;
+import fr.electronvert.facturation.model.releve.TypeReleve;
+
+import java.sql.*;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReleveDAOJdbc implements ReleveDAO {
+    @Override
+    public int save(Releve releve, int contratId) throws SQLException {
+        String query = "INSERT INTO releve (contrat_id, type_releve, date_releve, index_total, index_hp, index_hc) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection co = ConnectionManager.getConnection();
+             PreparedStatement ps = co.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, contratId);
+            ps.setString(2, releve.getTypeReleve().name());
+            ps.setDate(3, Date.valueOf(releve.getDateDeReleve()));
+            ps.setObject(4, releve.getIndex().get(TypeConso.TOTAL));
+            ps.setObject(5, releve.getIndex().get(TypeConso.HP));
+            ps.setObject(6, releve.getIndex().get(TypeConso.HC));
+            ps.executeUpdate();
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                }
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public List<Releve> findByContractId(int contractId) throws SQLException {
+        List<Releve> releves = new ArrayList<>();
+        String query = "SELECT * FROM releve WHERE contrat_id = ?";
+        try (Connection co = ConnectionManager.getConnection();
+        PreparedStatement ps = co.prepareStatement(query)){
+            ps.setInt(1, contractId);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()) {
+                    releves.add(releveFromResultSet(rs));
+                }
+            }
+        }
+        return releves;
+    }
+
+    @Override
+    public Releve findDernerByContractId(int contractId) throws SQLException {
+        String query = "SELECT * FROM releve WHERE contrat_id = ? ORDER BY date_releve DESC LIMIT 1";
+        try (Connection co = ConnectionManager.getConnection();
+        PreparedStatement ps = co.prepareStatement(query)){
+            ps.setInt(1, contractId);
+            try (ResultSet rs = ps.executeQuery()){
+                if (rs.next()) {
+                    return releveFromResultSet(rs);
+                }
+            }
+        }
+        return null;
+    }
+
+    private Releve releveFromResultSet(ResultSet rs) throws SQLException {
+        Map<TypeConso, Double> index = new EnumMap<>(TypeConso.class);
+        Double indexTotal = (Double) rs.getObject("index_total");
+        Double indexHp = (Double) rs.getObject("index_hp");
+        Double indexHc = (Double) rs.getObject("index_hc");
+        if (indexTotal != null) index.put(TypeConso.TOTAL, indexTotal);
+        if (indexHp != null) index.put(TypeConso.HP, indexHp);
+        if (indexHc != null) index.put(TypeConso.HC, indexHc);
+        return new Releve(
+                rs.getInt("id"),
+                TypeReleve.valueOf(rs.getString("type_releve")),
+                rs.getDate("date_releve").toLocalDate(),
+                index
+        );
+    }
+}
