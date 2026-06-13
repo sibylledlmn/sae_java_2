fabb5797eff7250906832acdc461e7f2eec92828
Sibylle <dillmann.sibylle@gmail.com>
2026-06-13 09:01:50 +0200

    feat(service): add UtilisateurService with modifierInfos and modifierMotDePasse

---
+++ ElectronVert/src/main/java/fr/electronvert/facturation/services/UtilisateurService.java
@@ -0,0 +1,35 @@
+package fr.electronvert.facturation.services;
+
+import fr.electronvert.facturation.dao.UtilisateurDAO;
+import fr.electronvert.facturation.model.utilisateur.Utilisateur;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
+
+public class UtilisateurService {
+
+    private final UtilisateurDAO utilisateurDAO;
+
+    public UtilisateurService(UtilisateurDAO utilisateurDAO) {
+        this.utilisateurDAO = utilisateurDAO;
+    }
+
+    public void modifierInfos(Utilisateur utilisateur, String prenom, String nom, String email) throws SQLException {
+        utilisateur.setPrenom(prenom);
+        utilisateur.setNom(nom);
+        utilisateur.setEmail(email);
+        utilisateurDAO.update(utilisateur);
+    }
+
+    public void modifierMotDePasse(Utilisateur utilisateur, String mdpActuel, String nouveauMdp, String confirmationMdp) throws SQLException {
+        if (!BCrypt.checkpw(mdpActuel, utilisateur.getMotDePasse())) {
+            throw new IllegalArgumentException("Mot de passe actuel incorrect");
+        }
+        if (!nouveauMdp.equals(confirmationMdp)) {
+            throw new IllegalArgumentException("Les mots de passe saisis ne correspondent pas.");
+        }
+        utilisateur.setMotDePasse(BCrypt.hashpw(nouveauMdp, BCrypt.gensalt()));
+        utilisateurDAO.update(utilisateur);
+    }
+
+}
